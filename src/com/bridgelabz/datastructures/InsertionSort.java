60e05d45ae05efae3d810589b6eb478a0962c162
sudha123 <sudhanaukala1997@gmail.com>
2022-08-31 17:07:06 +0530

    InsertionSort

---
+++ src/com/bridgelabz/datastructures/InsertionSort.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.datastructures;
+
+public class InsertionSort {
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.sort(arr);
+        printArray(arr);
+
+    }
+}
